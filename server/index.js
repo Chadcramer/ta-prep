9cc3c162868cbcef004420f00b53277cde5ab4f8
Chad Cramer <chcr6493@colorado.edu>
2019-05-12 15:20:47 -0500

    sunday Twitter app practice

--- server/index.js
+++ server/index.js
@@ -2,47 +2,24 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
-const database = require('../database/index.js');
+const bodyParser = require('body-parser')
 
-// middleware 
-    // body-parser 
-    app.use(bodyParser.json());
-    //CORS 
+// middleware
+    // CORS
     app.use(function(req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         next();
-      });
+    });
+    // support parsing of application/json type post data
+    app.use(bodyParser.json());
+    //support parsing of application/x-www-form-urlencoded post data
+    app.use(bodyParser.urlencoded({ extended: true }));
 
 // routes
-app.get('/tinder', (req, res) => {
-    let myQuery = `SELECT * FROM twitter ORDER BY ID DESC;`
-    database.query(myQuery, function (error, results) {
-        if (error){
-            console.log('post error from database: ', error);
-        } else {
-            console.log('post success from database:', results);
-            res.send(results);
-        }
-    });
+app.get('/twitter', (req, res) => res.send('Hello get!'));
+app.post('/twitter', (req, res) => res.send('Hello post!'));
 
-});
+// listen
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-app.post('/tinder', (req, res) => {
-
-    let myQuery = `INSERT INTO twitter (username, tweet)
-    VALUES ("${req.body.username}", "${req.body.tweet}");`;
-
-    database.query(myQuery, function (error, results) {
-        if (error){
-            console.log('post error from database: ', error);
-        } else {
-            console.log('post success from database:', results);
-        }
-    });
-    res.send()
-});
-
-// listen 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/index.js
+++ src/index.js
@@ -1,85 +1,48 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
-import Button from './ButtonComponent';
-import Form from './FormComponent';
 
 class App extends React.Component {
-
     constructor(props) {
-        super(props)
+        super(props);
         this.state = { 
             username: '',
             tweet: '',
-            feed: [],
-            tweetCounter: 0,
-        }
-        this.handlerUsernameState = this.handlerUsernameState.bind(this);
-        this.handlerTweetState = this.handlerTweetState.bind(this);
-        this.handlerSubmitbutton = this.handlerSubmitbutton.bind(this);
+        };
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleUsername = this.handleUsername.bind(this);
+        this.handleTweet = this.handleTweet.bind(this);
     }
 
-    handlerUsernameState (e) {
-        console.log(e.target.value);
-        this.setState({username: e.target.value});
-    }
-
-    handlerTweetState (e) {
-        console.log(e.target.value);
-        this.setState({tweet: e.target.value});
-    }
-
-    handleGET () {
-        axios.get('http://localhost:3000/tinder')
-        .then( (response) => {
-            console.log(response.data);
-            this.setState({feed: response.data});
-            this.setState({tweetCounter: this.state.feed.length});
-        })
-        .catch( (error) => {
-            console.log(error);
-        });
-    }
-
-    handlerSubmitbutton (e) {
-
+    handleSubmit (e) {
         e.preventDefault();
-
-        axios.post('http://localhost:3000/tinder', {
-            username: this.state.username,
-            tweet: this.state.tweet,
-            })
-            .then( (response) => {
-                this.handleGET();
-            console.log("post to server was a success");
-            })
-            .catch( (error) => {
-            console.log("error posting to server: ", error);
-        })
-
-        document.getElementById('username').value = null;
-        document.getElementById('tweet').value = null;
-
+        console.log('submit button working');
     }
 
-    componentDidMount() {
-        this.handleGET();
+    handleUsername (e) {
+        this.setState({ username: e.target.value });
     }
 
+    handleTweet (e) {
+        this.setState({ tweet: e.target.value });
+    }
+    
     render () {
-
       return (
-        <div id='form'>
-            <form>
-                < Form username={ this.handlerUsernameState.bind(this) } tweet={ this.handlerTweetState.bind(this) } />
-                < Button click={ this.handlerSubmitbutton.bind(this) } />
-                Number of Tweets: {this.state.tweetCounter} 
-            </form>
-            
-            { this.state.feed.map((e) => <h4> { e.username } {e.tweet} </h4> )} 
-
-        </div>
+      <div className='container'>
+        <h1>Twitter</h1>
+        <form>
+            Username:<br />
+            <input id="username" type="text" name="firstname" onChange={ this.handleUsername } />
+            <br />
+            Tweet:<br />
+            <input id="tweet" type="text" name="lastname" onChange={ this.handleTweet }/>
+            <br />
+            <input type="submit" value="Submit" onClick={ this.handleSubmit } />
+            <br />
+            Feed 
+            < hr />
+        </form>
+      </div>
       )}
   }
-
   ReactDOM.render(< App />, document.getElementById('root'));
